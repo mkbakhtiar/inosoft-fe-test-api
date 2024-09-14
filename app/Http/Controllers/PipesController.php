23b969f3225dc9e57582eb63595c8ccf1c8eca06
mkbakhtiar <mkbakhtiar.new@gmail.com>
2024-09-15 06:20:19 +0700

    create data filter based  on the previous selections priority and Loging

--- app/Http/Controllers/PipesController.php
+++ app/Http/Controllers/PipesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Log;
 
 class PipesController extends Controller
 {
@@ -12,12 +13,23 @@ class PipesController extends Controller
 
     public function __construct()
     {
-        // Create this variable for getting the data json base
         $this->pipesData = Storage::json('data-inventories.json');
+        // Add debugging to check if data is loaded correctly
+        Log::info('Pipes data loaded:', ['count' => count($this->pipesData['data'] ?? [])]);
     }
 
-    public function getFilters() {
-        // Get initial for filter field
+    public function getFilters(Request $request)
+    {
+        $selectedFilters = [
+            'product_type' => $request->query('product_type'),
+            'grade' => $request->query('grade'),
+            'connection' => $request->query('connection'),
+            'size' => $request->query('size')
+        ];
+
+        // Log the selected filters for debugging
+        Log::info('Selected filters:', $selectedFilters);
+
         $filters = [
             'product_type' => [],
             'grade' => [],
@@ -25,15 +37,67 @@ class PipesController extends Controller
             'size' => []
         ];
 
-        // Each data and matches for product_type, grade, connection and size
+        $matchCount = 0;
         foreach ($this->pipesData['data'] as $item) {
-            foreach ($filters as $key => $value) {
-                if (!in_array($item[$key], $filters[$key])) {
-                    $filters[$key][] = $item[$key];
+            $matchesAllFilters = true;
+
+            foreach ($selectedFilters as $key => $value) {
+                if ($value && strtolower($item[$key]) !== strtolower($value)) {
+                    $matchesAllFilters = false;
+                    break;
+                }
+            }
+
+            if ($matchesAllFilters) {
+                $matchCount++;
+                foreach ($filters as $key => $value) {
+                    if (!in_array($item[$key], $filters[$key])) {
+                        $filters[$key][] = $item[$key];
+                    }
                 }
             }
         }
 
-        return response()->json(['filters' => $filters]);
+        // Log the match count and resulting filters
+        Log::info('Matches found:', ['count' => $matchCount]);
+        Log::info('Resulting filters:', $filters);
+
+        foreach ($filters as &$options) {
+            sort($options);
+        }
+
+        return response()->json([
+            'filters' => $filters,
+            'selected' => $selectedFilters,
+        ]);
+    }
+
+    public function search(Request $request)
+    {
+        $filters = [
+            'product_type' => $request->query('product_type'),
+            'grade' => $request->query('grade'),
+            'connection' => $request->query('connection'),
+            'size' => $request->query('size')
+        ];
+
+        // Log the search filters
+        Log::info('Search filters:', $filters);
+
+        $results = collect($this->pipesData['data'])->filter(function ($item) use ($filters) {
+            foreach ($filters as $key => $value) {
+                if (!empty($value) && strtolower($item[$key]) != strtolower($value)) {
+                    return false;
+                }
+            }
+            return true;
+        })->values();
+
+        // Log the number of results
+        Log::info('Search results count:', ['count' => $results->count()]);
+
+        return response()->json([
+            'results' => $results,
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PipesController;
 
-Route::get('/filters', [PipesController::class, 'getFilters']);
+Route::get('/filters', [PipesController::class, 'getFilters']); // route api for get data filters
+Route::get('/search', [PipesController::class, 'search']); // route api for call api search with some parameters
